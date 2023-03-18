a7f54b7f94a059bf50c05165e7bbba1b195ba6b4
internetisgone <schumann777@proton.me>
2023-03-18 20:35:54 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import os
+import openai 
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+bot_command_prefix = "!c"
+
+def send_msg_openai(prompt):
+    try: 
+        completion = openai.ChatCompletion.create(
+            model = "gpt-4",
+            messages = [ 
+                {"role": "user", "content": prompt} 
+            ],
+            temperature = 0.9,
+        )
+        print(completion.choices[0])
+        return completion.choices[0].message.content
+    except Exception as e:
+        print(e)
+        return ""
+
+
+async def send_msg_discord(message, user_message):
+    try:
+        # response = send_msg_openai(user_message)
+        response = "kill yourself" # test 
+        message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents = intents, proxy="http://127.0.0.1:1087")  # proxy is used for bypassing internet censorship. can be ommited if discord isn't banned in your country
+
+    @client.event
+    async def on_ready():
+         print(f"{client.user} is running")
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == bot_command_prefix:
+            user_message = user_message[len(bot_command_prefix):]
+            await send_msg_discord(message, user_message)
+
+    client.run(os.getenv("DISCORD_KEY"))
+
+
+if __name__ == '__main__':
+    print("running...")
+    run_discord_bot()
+    # response = send_msg_openai()
